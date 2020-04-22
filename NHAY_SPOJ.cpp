48679fd18cdeef07883c86d45370d7cd18a0e443
KausBoss <ktkaustubh4@gmail.com>
2020-04-22 20:06:44 +0530

    3

--- NHAY_SPOJ.cpp
+++ NHAY_SPOJ.cpp
@@ -1,84 +1,56 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+#define ll long long
 
-void MakeTable(string p, int table[]){
-	int n=p.length();
+
+void KMP(string needle, string hay, int n){
+	//first making the pi table
+	int table[n]={0};
 	int i=0, j=1;
+	bool flag=false;
 	while(j<n){
-		if(p[i]!=p[j]){
-			if(i==0){
-				j++;
-			}
-			else{
-				i=0;
-			}
-		}
-		else{
+		if(needle[i] == needle[j]){
 			table[j]=i+1;
 			i++;
-			j++;
 		}
+		else if(needle[i]!=needle[j] && i>0){
+			i=0;
+		}
+		j++;
 	}
-	// for(int i=0; i<n; i++){
-	// 	cout<<table[i]<<" ";
-	// }
-}
-
-bool KMP(string s, string p, int table[]){
-	bool what=true;
-	int i=0; int j=0;
-	int ls=s.length();
-	int lp=p.length();
-
-	while(i<ls){
-		while(j>0&& s[i]!=p[j]){
-			j = table[j-1];
+	i=0;
+	j=0;
+	while(i<hay.length()){
+		if(hay[i]!=needle[j] && j>0){
+			j=table[j-1];
 		}
-
-		if(s[i]==p[j]){
-			i++;
-			j++;
+		if(hay[i] == needle[j]){
+			i++;j++;
 		}
-		else{
+		else {
 			i++;
 		}
-		if(j==lp){
-			bool what=false;
-			cout<<i-j<<"\n";
-			j= table[j-1];
+		if(j==n){
+			flag = true;
+			cout<<i-j<<endl;
+			j=table[j-1];
 		}
 	}
-	return what;
+	if(!flag){cout<<endl;}
 }
 
 
 int main(){
 	ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);
-	// string p, s;
-	// // int t;
-	// // cin>>t;
-	// // while(t--){
-	// int len;
-	// cin>>len>>p>>s;
-	// MakeTable(p, table);
-	// if(KMP(s, p, table)){
-	// 	cout<<"\n";
-	// }
-	int m;
-    bool first = true;
-    while (scanf("%d",&m)==1) {
-	    if (!first) puts("");
-	    first = false;
-	    int table[m]={0};
-	    string needle; //getline(cin, needle);
-	    getline(cin, needle);
-	    MakeTable(needle, table);
-	    string haystack;
-	    getline(cin, haystack);
-	    KMP(haystack, needle, table);
-  	}
-  return 0;
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+	int n;
+	string needle, hay;
+	while(cin>>n>>needle>>hay){
+		KMP(needle, hay, n);
+	}
 }
